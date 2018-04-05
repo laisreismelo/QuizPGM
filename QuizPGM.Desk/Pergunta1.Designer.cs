e91e297aab301944fad9ca4fe3e98b3c268cc25b
Laís Reis Melo <laisreismelo2002@gmail.com>
2018-04-05 11:29:47 -0300

    inicio da p1

--- QuizPGM.Desk/Pergunta1.Designer.cs
+++ QuizPGM.Desk/Pergunta1.Designer.cs
@@ -28,61 +28,118 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(pergunta1));
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.questão1 = new System.Windows.Forms.GroupBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.groupBox1.SuspendLayout();
+            this.a1 = new System.Windows.Forms.RadioButton();
+            this.d1 = new System.Windows.Forms.RadioButton();
+            this.b1 = new System.Windows.Forms.RadioButton();
+            this.c1 = new System.Windows.Forms.RadioButton();
+            this.questão1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // groupBox1
+            // questão1
             // 
-            this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.ForeColor = System.Drawing.Color.Black;
-            this.groupBox1.Location = new System.Drawing.Point(38, 39);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(200, 140);
-            this.groupBox1.TabIndex = 0;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = " Questão 1 ";
-            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter_1);
+            this.questão1.BackColor = System.Drawing.Color.Gainsboro;
+            this.questão1.Controls.Add(this.d1);
+            this.questão1.Controls.Add(this.c1);
+            this.questão1.Controls.Add(this.a1);
+            this.questão1.Controls.Add(this.b1);
+            this.questão1.Controls.Add(this.label1);
+            this.questão1.ForeColor = System.Drawing.Color.Black;
+            this.questão1.Location = new System.Drawing.Point(305, 173);
+            this.questão1.Name = "questão1";
+            this.questão1.Size = new System.Drawing.Size(311, 162);
+            this.questão1.TabIndex = 0;
+            this.questão1.TabStop = false;
+            this.questão1.Text = " Questão 1 ";
+            this.questão1.Enter += new System.EventHandler(this.groupBox1_Enter_1);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.ForeColor = System.Drawing.Color.Black;
-            this.label1.Location = new System.Drawing.Point(6, 33);
+            this.label1.Location = new System.Drawing.Point(6, 25);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(193, 104);
+            this.label1.Size = new System.Drawing.Size(263, 26);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Qual dos gases abaixo não foi utilizado \r\ncomo arma de destruição em massa \r\ndura" +
-    "nte a 1º Guerra:\r\n\r\na) Gás cloro\r\nb) Gás mostarda\r\nc) Gás fosfogênio\r\nd) Gás lac" +
-    "rimogênio\r\n";
+            this.label1.Text = "Qual dos gases abaixo não foi utilizado como arma de \r\ndestruição em massa durant" +
+    "e a 1º Guerra:\r\n";
             this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // a1
+            // 
+            this.a1.AutoSize = true;
+            this.a1.Location = new System.Drawing.Point(9, 67);
+            this.a1.Name = "a1";
+            this.a1.Size = new System.Drawing.Size(82, 17);
+            this.a1.TabIndex = 1;
+            this.a1.TabStop = true;
+            this.a1.Text = "a) Gás cloro\r\n";
+            this.a1.UseVisualStyleBackColor = true;
+            this.a1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // d1
+            // 
+            this.d1.AutoSize = true;
+            this.d1.Location = new System.Drawing.Point(9, 136);
+            this.d1.Name = "d1";
+            this.d1.Size = new System.Drawing.Size(118, 17);
+            this.d1.TabIndex = 1;
+            this.d1.TabStop = true;
+            this.d1.Text = "d) Gás lacrimogênio";
+            this.d1.UseVisualStyleBackColor = true;
+            // 
+            // b1
+            // 
+            this.b1.AutoSize = true;
+            this.b1.Location = new System.Drawing.Point(9, 90);
+            this.b1.Name = "b1";
+            this.b1.Size = new System.Drawing.Size(102, 17);
+            this.b1.TabIndex = 2;
+            this.b1.TabStop = true;
+            this.b1.Text = "b) Gás mostarda";
+            this.b1.UseVisualStyleBackColor = true;
+            // 
+            // c1
+            // 
+            this.c1.AutoSize = true;
+            this.c1.Location = new System.Drawing.Point(9, 113);
+            this.c1.Name = "c1";
+            this.c1.Size = new System.Drawing.Size(108, 17);
+            this.c1.TabIndex = 3;
+            this.c1.TabStop = true;
+            this.c1.Text = "c) Gás fosfogênio";
+            this.c1.UseVisualStyleBackColor = true;
+            this.c1.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
             // 
             // pergunta1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.ClientSize = new System.Drawing.Size(594, 379);
-            this.Controls.Add(this.groupBox1);
+            this.ClientSize = new System.Drawing.Size(974, 652);
+            this.Controls.Add(this.questão1);
             this.ForeColor = System.Drawing.SystemColors.ControlText;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "pergunta1";
             this.Text = "Pergunta - 01";
             this.TransparencyKey = System.Drawing.Color.Transparent;
             this.Load += new System.EventHandler(this.Questão1_Load);
-            this.groupBox1.ResumeLayout(false);
-            this.groupBox1.PerformLayout();
+            this.questão1.ResumeLayout(false);
+            this.questão1.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox questão1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton a1;
+        private System.Windows.Forms.RadioButton b1;
+        private System.Windows.Forms.RadioButton d1;
+        private System.Windows.Forms.RadioButton c1;
     }
 }
 
--- QuizPGM.Desk/Principal.Designer.cs
+++ QuizPGM.Desk/Principal.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace QuizPGM.Desk
 {
-    partial class Form1
+    partial class Principal
     {
         /// <summary>
         /// Variável de designer necessária.
@@ -37,6 +37,7 @@
             this.ClientSize = new System.Drawing.Size(474, 393);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
