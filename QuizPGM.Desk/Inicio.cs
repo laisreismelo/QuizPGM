108d21d1bce8edf082244f554332c1035790e80b
Felipe Teixeira <36284538+Felipe4747@users.noreply.github.com>
2018-04-26 08:23:19 -0300

    Bugfix

--- QuizPGM.Desk/Inicio.cs
+++ QuizPGM.Desk/Inicio.cs
@@ -17,6 +17,7 @@ namespace QuizPGM.Desk
         public Inicio()
         {
             InitializeComponent();
+            res = "n";
         }
 
         private void inicio_Load(object sender, EventArgs e)
